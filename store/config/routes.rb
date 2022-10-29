ebb5d1bcc5e2a17b3b211de3e728893246a66dd8
diogomarta907 <diogo@LAPTOP-GO0IHH6T>
2022-10-29 20:55:52 +0100

    create models and add index view

--- store/config/routes.rb
+++ store/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "storage#index"
+  
+  get "/storage", to: "storage#index"
+  resources :storage
+  resources :country
+  resources :city
+  resources :color
+  resources :materials
 end
